617043ab08d6c9b0e6a662893ba7408a87dbdb70
MX <mxber2022@gmail.com>
2025-08-09 19:11:59 +0700

    Create layout.tsx

---
+++ TradeTok/app/(app)/layout.tsx
@@ -0,0 +1,7 @@
+export default function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="w-full max-w-md mx-auto px-4 pt-3 pb-24">{children}</div>
+  );
+}
+
+
